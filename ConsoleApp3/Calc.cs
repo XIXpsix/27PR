22eef4221fc253a447a5cf119c0b8aa8cba8ee56
XIXpsix <lebedevdima902@gmail.com>
2025-03-06 10:48:09 +0200

    Add files via upload

---
+++ ConsoleApp3/Calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Calc
+    {        
+            private double x;
+            private double y;
+            public double X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+
+            public double Y
+            {
+                get { return y; }
+                set { y = value; }
+            }
+            public Calc()
+            {
+                x = 0;
+                y = 0;
+            }
+            public Calc(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public void PrintCoordinates()
+            {
+                Console.WriteLine($"Точка: ({x}, {y})");
+            }
+    }
+}
